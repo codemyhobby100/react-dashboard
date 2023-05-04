c82909ec6d7925ab2a640fd5da5ba20d453f90b0
Codemyhobby <alhassandamilola16@gmail.com>
2023-05-04 15:25:24 +0100

    update

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -37,8 +37,8 @@ const Layout = () => {
           <div className={css.profile}>
             <img src="./profile.png" alt="person image" />
             <div className={css.details}>
-              <span>Denis Steven</span>
-              <span>devissteven@gmail.com</span>
+              <span>Jim Samuel</span>
+              <span>jimsamuel@gmail.com</span>
             </div>
           </div>
 
